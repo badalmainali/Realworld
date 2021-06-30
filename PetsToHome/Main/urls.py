89e892ccf056d576643d8e73e99f6f125b024dca
badalmainali <badalma12@gmail.com>
2021-06-30 22:25:11 +0545

    web

--- PetsToHome/Main/urls.py
+++ PetsToHome/Main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.main,name="main")
+    path('',views.main,name="main"),
+    path('about',views.about,name="about"),
+    path('contact',views.contact,name="contact"),
 ]
